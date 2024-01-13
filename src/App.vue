8bfe19880de22c28c45c2c08456bbd54b62a45ac
Maxim Belkin <maximbe@ext.bershka.com>
2024-01-13 20:02:28 +0100

    feat: cleanup and docs page

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,66 @@
-<script setup lang="ts">
+<script>
+import { defineComponent } from 'vue'
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    RouterLink,
+    RouterView
+  },
+  setup() {
+    const linkList = [{ title: '📁 Docs', to: '/' }]
+
+    return {
+      linkList
+    }
+  }
+})
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
+  <header class="header">
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+      <nav class="navigation">
+        <RouterLink v-for="link in linkList" :key="link.title" :to="link.to">
+          {{ link.title }}
+        </RouterLink>
       </nav>
     </div>
   </header>
 
-  <RouterView />
+  <main class="main">
+    <RouterView />
+  </main>
 </template>
 
-<style scoped>
-header {
+<style lang="scss" scoped>
+.header {
   line-height: 1.5;
   max-height: 100vh;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
+  .navigation {
+    width: 100%;
+    font-size: 12px;
+    border: 1px solid var(--color-border);
+    margin-bottom: 1rem;
     padding: 1rem 0;
-    margin-top: 1rem;
+    border-radius: 10px;
+
+    a {
+      display: inline-block;
+      padding: 0 1rem;
+      border-left: 1px solid var(--color-border);
+      text-decoration: none;
+
+      .router-link-exact-active {
+        color: var(--color-text);
+      }
+
+      &:first-of-type {
+        border: none;
+      }
+    }
   }
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,81 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'HomeView',
+  setup() {
+    const linkList = [
+      ['📁 Basics', [
+        ['📄 Reactivity Fundamentals', 'https://vuejs.org/guide/essentials/reactivity-fundamentals.html#declaring-reactive-state'],
+        ['📄 Props', 'https://vuejs.org/guide/components/props.html'],
+        ['📄 Events', 'https://vuejs.org/guide/components/props.html'],
+        ['📄 Methods', 'https://vuejs.org/guide/essentials/reactivity-fundamentals.html#declaring-methods'],
+        ['📄 Computed', 'https://vuejs.org/guide/essentials/computed.html#basic-example'],
+        ['📄 Watchers', 'https://vuejs.org/guide/essentials/watchers.html#watchers'],
+        ['📄 Template Refs', 'https://vuejs.org/guide/essentials/template-refs.html#template-refs'],
+        ['📄 Form Input Bindings', 'https://vuejs.org/guide/essentials/forms.html'],
+        ['📄 Lifecycle Hooks', 'https://vuejs.org/guide/essentials/lifecycle.html'],
+        ['📄 Async Components', 'https://vuejs.org/guide/components/async.html#basic-usage']
+      ]],
+      ['📁 Advanced', [
+        ['📄 Composable', 'https://vuejs.org/guide/reusability/composables.html'],
+        ['📄 Transitions', 'https://vuejs.org/guide/built-ins/transition.html'],
+        ['📄 Teleport', 'https://vuejs.org/guide/built-ins/teleport.html'],
+        ['📄 Slots', 'https://vuejs.org/guide/components/slots.html'],
+        ['📄 Directives', 'https://vuejs.org/api/built-in-directives.html#built-in-directives'],
+        ['📄 Plugins', 'https://vuejs.org/guide/reusability/plugins.html#plugins'],
+        ['📄 Vuex Store', 'https://bluuweb.github.io/vue-udemy/21-vuex-composition/#api']
+      ]]
+    ]
+
+    return {
+      linkList
+    }
+  }
+})
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="wrapper">
+    <fieldset v-for="[category, subjectList] in linkList" :key="category" class="category-block">
+      <legend class="category-title">{{ category }}</legend>
+
+      <ul class="link-list">
+        <li v-for="[title, url] in subjectList" :key="title" class="link-item">
+          <a :href="url" target="_blank">{{ title }}</a>
+        </li>
+      </ul>
+    </fieldset>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+}
+
+.category-block {
+  border: 1px solid var(--color-border);
+  padding: 8px 14px;
+  border-radius: 10px;
+
+  .category-title {
+    padding: 0 2px;
+    font-size: 12px;
+  }
+}
+
+.link-list {
+  padding-left: 2rem;
+
+  .link-item {
+    list-style: none;
+
+    a {
+      font-size: 12px;
+    }
+  }
+}
+</style>
