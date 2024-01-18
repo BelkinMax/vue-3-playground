37cdce63ca0a08380a3218a5fb8a74e3f399edcc
Fernando Hernáez López <fernandohlo@ext.bershka.com>
2024-01-18 16:45:07 +0100

    <3

---
+++ src/assignments/basic/ReuseComponents/ProductCard.vue
@@ -0,0 +1,42 @@
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'ProductCard',
+  props: {
+    product: {
+      type: Object,
+      required: true,
+      default: () => {}
+    }
+  },
+  emits: ['productAdded'],
+  setup (props, ctx) {
+    
+    function productAdded() {
+      ctx.emit('productAdded', props.product)
+    }
+
+    return {
+      productAdded
+    }
+  }
+})
+</script>
+
+<template>
+  <div class="product-card">
+    <div class="thumbnail">
+      <img :src="product.image" :alt="product.title" />
+    </div>
+    <div class="content">
+      <h2 class="title">{{ product.title }}</h2>
+      <p class="price">{{ product.price }}$</p>
+    </div>
+    <div class="cta">
+      <button @click="productAdded()">+</button>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" src="./reuse-components.scss" />
