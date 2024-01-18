270bd92dc31cd69d7eecf6f40f08b99acdc310d4
Maxim Belkin <maximbe@ext.bershka.com>
2024-01-18 12:32:09 +0100

    feat: calculator app

---
+++ src/assignments/basic/CalculatorApp/components/PadButton.vue
@@ -0,0 +1,28 @@
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {
+    function emitClick() {
+      // TODO: emit 'buttonClick'
+    }
+
+    return {
+      button: {},
+      emitClick
+    }
+  }
+})
+</script>
+
+<template>
+  <button
+    class="button"
+    :class="{
+      [button.classes]: true
+    }"
+    @click="emitClick"
+  >
+    {{ button.label }}
+  </button>
+</template>
