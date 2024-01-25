820085aa4bfb7e96888de33b2f2e23c3b75629cf
Juan Manuel López Pazos <juanmlpa@ext.bershka.com>
2024-01-25 15:30:20 +0100

    feat: add product card with api call and add product function

---
+++ src/assignments/basic/ReuseComponents/ProductCard.vue
@@ -0,0 +1,49 @@
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'ProductCard',
+  props: {
+    id: {
+      type: Number,
+      required: true
+    },
+    image: {
+      type: String,
+      required: true
+    },
+    price: {
+      type: Number,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    }
+  },
+  setup () {
+    function addProduct() {
+      console.log(`${this.id}: ${this.title} added!`)
+    }
+
+    return {
+      addProduct
+    };
+  }
+})
+</script>
+
+<template>
+  <div class="product-card" :key="id">
+    <div class="thumbnail">
+      <img :src="image" :alt="title" />
+    </div>
+    <div class="content">
+      <h2 class="title">{{ title }}</h2>
+      <p class="price">{{ price }}$</p>
+    </div>
+    <div class="cta">
+      <button @click="addProduct">+</button>
+    </div>
+  </div>
+</template>
