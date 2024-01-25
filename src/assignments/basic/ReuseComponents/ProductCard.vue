36057c59ba289aa6792a7550e392d95ed381db97
Ignacio Olalde Ramos <ignacioor@ext.bershka.com>
2024-01-25 15:24:58 +0100

    basics

---
+++ src/assignments/basic/ReuseComponents/ProductCard.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { defineEmits, defineProps } from 'vue';
+
+const props = defineProps({ product: Object });
+const emit = defineEmits(['productAdd']);
+
+function onProductAddClick () {
+  emit('productAdd');
+}
+</script>
+
+<template>
+  <div class="product-card">
+    <div class="thumbnail">
+      <img :src="props.product.image" :alt="props.product.title" />
+    </div>
+    <div class="content">
+      <h2 class="title">{{ props.product.title }}</h2>
+      <p class="price">{{ props.product.price }}$</p>
+    </div>
+    <div class="cta">
+      <button @click="onProductAddClick">+</button>
+    </div>
+  </div>
+</template>
--- src/assignments/basic/ReuseComponents/index.vue
+++ src/assignments/basic/ReuseComponents/index.vue
@@ -1,39 +1,39 @@
 <script>
-import { defineComponent } from 'vue'
-// import { useApi } from './helpers/useApi'
+import { defineComponent, ref, onMounted } from 'vue'
+import { useApi } from './helpers/useApi'
+import ProductCard from './ProductCard.vue';
 
 export default defineComponent({
   name: 'ReuseComponents',
+  components: { ProductCard },
   setup() {
-    //TODO:  const api = useApi();
-    const products = [] // await api.fetchProducts()
+    const api = useApi();
+    const products = ref([]);
 
     function addProduct(product) {
       console.log(`${product.id}: ${product.title} added!`)
     }
+
+    onMounted(async () => {
+      products.value = await api.fetchProducts();
+    });
 
     return {
       products,
       addProduct
     }
   }
-})
+});
 </script>
 
 <template>
   <div class="product-grid">
-    <div class="product-card" v-for="product in products" :key="product.id">
-      <div class="thumbnail">
-        <img :src="product.image" :alt="product.title" />
-      </div>
-      <div class="content">
-        <h2 class="title">{{ product.title }}</h2>
-        <p class="price">{{ product.price }}$</p>
-      </div>
-      <div class="cta">
-        <button @click="addProduct">+</button>
-      </div>
-    </div>
+    <product-card
+      v-for="product in products"
+      :key="product.id"
+      :product="product"
+      @product-add="addProduct(product)"
+    />
   </div>
 </template>
 
