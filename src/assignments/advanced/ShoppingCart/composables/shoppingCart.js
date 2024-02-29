57e8a266b982d347ff8633f637c193ac5b440c3b
Félix Andreassi <felixa@ext.bershka.com>
2024-02-29 16:48:42 +0100

    feature(shoppingCart): ShoppingCart Exercise

--- src/assignments/advanced/ShoppingCart/composables/shoppingCart.js
+++ src/assignments/advanced/ShoppingCart/composables/shoppingCart.js
@@ -4,7 +4,30 @@
  * @property {string} name
  * @property {number} price
  */
+import {reactive, ref} from "vue";
 
 export default function useShoppingCart() {
-  return {}
+
+  const total = ref(0)
+  const cart = reactive([]);
+
+  const addToCart = product => {
+    console.log(product)
+    cart.push(product)
+    total.value += product.price;
+  };
+
+  const removeFromCart = (productId) => {
+    const indexToDrop = cart.findIndex(item => productId === item.id)
+    const productPrice = cart[indexToDrop].price;
+    cart.splice(indexToDrop, 1);
+    total.value -= productPrice;
+  }
+
+  return {
+    addToCart,
+    removeFromCart,
+    cart,
+    total
+  }
 }
