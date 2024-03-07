47e33eaf57e5f47a281191aea37612774dc65541
Beto <beto_2613@hotmail.com>
2024-03-07 16:25:29 +0100

    [feature/vue-3-playground] fix(alert) handle composobale for save in storage and state with computed validator required, also add conditional for component disabled for error case

--- src/assignments/advanced/ShowAlert/alert.composable.js
+++ src/assignments/advanced/ShowAlert/alert.composable.js
@@ -1,12 +1,21 @@
+import { computed, ref } from 'vue'
 import { useStorage } from '@vueuse/core';
 
 // https://vueuse.org/core/useStorage/
 export default function useAlert (alertKey, initialData, validator) {
-  // TODO: use vueuse/useStorage
+  const state = useStorage(alertKey, initialData)
+  const data = ref(state.value)
+  const isRequired = computed(() => validator(data))
+
+
+  function updateData (newData) {
+    data.value = newData
+    state.value = newData
+  }
 
   return {
-    data: {},
-    isRequired: true,
-    updateData: () => {}
+    data,
+    isRequired,
+    updateData
   }
 }
--- src/assignments/advanced/ShowAlert/index.vue
+++ src/assignments/advanced/ShowAlert/index.vue
@@ -1,5 +1,5 @@
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent, computed, ref } from 'vue'
 import TheAlert from './components/TheAlert.vue';
 import useAlert from './alert.composable.js'
 
@@ -9,10 +9,11 @@ export default defineComponent({
     TheAlert
   },
   setup() {
+    const MAX_CLICKS = 5;
     const { data, isRequired, updateData } = useAlert(
       'clicks-alert',
       { clicks: 0 },
-      (data) => { return data?.value.clicks < 5 }
+      (data) => data.value.clicks < 5
     );
 
     /*
@@ -21,12 +22,16 @@ export default defineComponent({
     *  2. Show warning alert with removable option in global-alert slot (element id: global-alert)
     *  3. If alert was closed show non removable error alert inside the page
     */
-
+    const clicksLeft = computed(() => MAX_CLICKS - data.value.clicks)
+    const isError = ref(false)
+ 
     return {
+      data,
+      isError,
       isRequired,
-      clicksLeft: 5,
-      onOk: () => {},
-      onClose: () => {}
+      clicksLeft,
+      onOk: () => { updateData({ clicks: data.value.clicks + 1 }) },
+      onClose: () => { isError.value = true }
     }
   }
 })
@@ -34,13 +39,15 @@ export default defineComponent({
 
 <template>
   <div class="show-alert">
-    <TheAlert
-      kind="error"
-      is-removable
-      @ok="onOk"
-      @close="onClose"
-    >
-      Click "Ok" {{ clicksLeft }} times
-    </TheAlert>
+    <Teleport v-if="isRequired" to="#global-alert" :disabled="isError">
+      <TheAlert
+        :kind="isError ? 'error' : 'warning'"
+        :is-removable="!isError"
+        @ok="onOk"
+        @close="onClose"
+      >
+        Click "Ok" {{ clicksLeft }} times
+      </TheAlert>
+    </Teleport>
   </div>
 </template>
