af4c48861be2a295362d5a15ce03c9fdd4c15dc4
Maxim Belkin <maximbe@ext.bershka.com>
2024-03-07 14:39:44 +0100

    feat: dynamic components assignment

---
+++ src/assignments/advanced/AsyncComponents/components/BToR.vue
@@ -0,0 +1,39 @@
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'BToR'
+})
+</script>
+
+<template>
+  <div class="b-to-r">
+    <svg width="480" height="480" viewBox="0 0 480 480" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <mask id="mask0_261_37" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="-1" width="480" height="481">
+        <rect width="480" height="480" transform="matrix(1.19249e-08 -1 -1 -1.19249e-08 480 480)" fill="#D9D9D9"/>
+      </mask>
+      <g mask="url(#mask0_261_37)">
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M256.249 242.044L292.112 375.887L158.269 340.024L256.249 242.044ZM247.966 272.954L269.484 353.26L189.179 331.742L247.966 272.954Z" fill="#F5E8DD"/>
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M240.718 360L310 480L171.436 480L240.718 360ZM240.718 392L282.287 464L199.149 464L240.718 392Z" fill="#F5E8DD"/>
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M240.718 300L310 420L171.436 420L240.718 300ZM240.718 332L282.287 404L199.149 404L240.718 332Z" fill="#F5E8DD"/>
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M323.145 197.573L287.282 331.416L189.302 233.436L323.145 197.573ZM300.518 220.201L279 300.506L220.212 241.719L300.518 220.201Z" fill="#F5E8DD"/>
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M417.955 224.189L319.976 322.168L284.113 188.326L417.955 224.189ZM387.046 232.471L328.258 291.259L306.74 210.953L387.046 232.471Z" fill="#F5E8DD"/>
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M540 239.718L420 309L420 170.436L540 239.718ZM508 239.718L436 281.287L436 198.148L508 239.718Z" fill="#F5E8DD"/>
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M480 239.718L360 309L360 170.436L480 239.718ZM448 239.718L376 281.287L376 198.148L448 239.718Z" fill="#F5E8DD"/>
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M240.718 420L310 540L171.436 540L240.718 420ZM240.718 452L282.287 524L199.149 524L240.718 452Z" fill="#F5E8DD"/>
+      </g>
+    </svg>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.b-to-r {
+  display: flex;
+
+  svg {
+    aspect-ratio: 1;
+    height: 48px;
+    width: 48px;
+  }
+}
+</style>
